94db904f1420960aacd4580831f951006d0d9677
Anirban <anirban1996jana08@gmail.com>
2022-01-18 19:26:38 +0530

    adv frontend assignment2

---
+++ jan-08/quiz/game.js
@@ -0,0 +1,111 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+let questions = [
+  {
+    question: "Which country has implemented new laws banning child marriage?",
+    choice1: "Malaysia",
+    choice2: "Philippines",
+    choice3: "Indonesia",
+    choice4: "Sri Lanka",
+    answer: "Philippines"
+  },
+  {
+    question:
+      "Scientists have detected a new strain of Covid-19 that combines both Delta and Omicron in which nation?",
+    choice1: "Sweden",
+    choice2: "Denmark",
+    choice3: "Switzerland",
+    choice4: "Cyprus",
+    answer: "Cyprus"
+  },
+  {
+    question: " Which countries President has signed a law prohibiting Child Marriage?",
+    choice1: "UK",
+    choice2: "Philippines",
+    choice3: "UAE",
+    choice4: "USA",
+    answer: "Philippines"
+  },
+  {
+    question: " Atacama Large Millimeter/submillimeter Array (ALMA)' is located in which country?",
+    choice1: "Australia",
+    choice2: "Chile",
+    choice3: "USA",
+    choice4: "Russia",
+    answer: "Chile"
+  }
+];
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    //go to the end page
+    return window.location.assign("/end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  //Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startGame();
